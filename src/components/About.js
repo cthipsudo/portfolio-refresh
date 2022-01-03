42c6630f321cfadd0426c8de5864023a65a81c6b
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2022-01-03 11:41:18 -0600

    Added Header to About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,15 @@
-
 const About = () => {
-  return(
-    <section className="flex justify-evenly items-center">
-      <div className="col col1">
-        Column 1 Stuff here
+  return (
+    <section className="">
+      <div className="section-header flex justify-start px-4">
+        <h3 className="underline">About</h3>
       </div>
-      <div className="col col2">
-        Column 2 Stuff here
+      <div className="flex justify-evenly items-center">
+        <div className="col col1">Column 1 Stuff here</div>
+        <div className="col col2">Column 2 Stuff here</div>
       </div>
     </section>
   );
-}
+};
 
 export default About;
