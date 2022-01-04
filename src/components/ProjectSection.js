d8304cf73bac15377122d16302cfa37cc25e14dd
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2022-01-04 13:51:22 -0600

    added skill icon logos

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,11 +1,22 @@
 
+import SkillSetIcons from "../data/SkillSetIcons";
+import { v4 as uuidv4 } from "uuid";
+
 
 const ProjectSection = () => {
+
+  const skillIcons = SkillSetIcons.map((item) => {
+    return <img className="skillset-icon" key={uuidv4()} src={require(`../images/${item.fileName}`)} alt={`item.alt`}/>
+  });
+
   return(
     <section>
       <div className="project-header flex justify-between px-4">
         <h3 className="underline">Projects</h3>
-        <div>Skill Icons here.</div>
+        <div className="flex items-center gap-2">
+          <h4 className="">Skillset:</h4>
+          {skillIcons}
+        </div>
       </div>
 
       <div>
---
+++ src/data/SkillSetIcons.js
@@ -0,0 +1,32 @@
+const SkillSetIcons = [
+  {
+    fileName: 'css3-icon.webp',
+    altText: 'css-3-logo'
+  },
+  {
+    fileName: 'html5-icon.webp',
+    altText: 'html-5-logo'
+  },
+  {
+    fileName: 'js-icon.webp',
+    altText: 'javascript-logo'
+  },
+  {
+    fileName: 'react-icon.webp',
+    altText: 'react-framework-logo'
+  },
+  {
+    fileName: 'tailwind-css-icon.webp',
+    altText: 'tailwind-css-logo'
+  },
+  {
+    fileName: 'webpack-icon.webp',
+    altText: 'webpack-logo'
+  },
+  {
+    fileName: 'photoshop-icon.webp',
+    altText: 'photoshop-logo'
+  },
+];
+
+export default SkillSetIcons;
