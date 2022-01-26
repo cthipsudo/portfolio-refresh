2702862ebbad868dec54e81d5e181d59af106e8e
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2022-01-26 12:43:24 -0600

    increased the container width and changed hero content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,19 @@
-
-
-
 const Hero = () => {
-  return(
-    <section>
-      <h1>Hey there! I'm Chanpasong O.<br/>Thipphakhinkeo, call me Oscar.</h1>
-      <h2>I'm a Software Developer living in Amarillo, Texas and I make cool sites!</h2>
+  return (
+    <section className="text-left">
+      <div className="section-wrapper-inner">
+        <h1>
+          Hey there! I'm Chanpasong O.
+          <br />
+          Thipphakhinkeo, call me Oscar.
+        </h1>
+        <h2>
+          I'm a Frontend Developer living in Amarillo, Texas and <br/>I create custom
+          sites to suite personal and professional needs!
+        </h2>
+      </div>
     </section>
   );
-}
+};
 
 export default Hero;
