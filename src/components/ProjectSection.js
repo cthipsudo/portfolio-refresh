429d241901f2be46f63135ecacd18b7a017faaa1
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2022-01-26 13:51:52 -0600

    changed mobile stylings for project section

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -40,23 +40,27 @@ const ProjectSection = () => {
   return (
     <section className="project-section">
       <div className="section-wrapper-inner">
-        <div className="project-header flex flex-col items-center md:items-start">
+        <div className="project-header flex flex-col items-start md:items-start">
           <h3 className="underline">Projects</h3>
         </div>
 
-        <div className="project-wrapper flex flex-wrap justify-center md:justify-start gap-4">
+        <div className="project-wrapper flex flex-wrap justify-start gap-4">
           {Projects}
         </div>
 
         <div className="skills-wrapper">
-          <div className="flex flex-col md:items-center md:flex-row md:justify-between">
-            <div className="flex flex-col items-center md:flex-row gap-2">
+          <div className="flex flex-col items-start md:items-center md:flex-row md:justify-between">
+            <div className="flex flex-col items-start md:flex-row gap-2 mt-10 md:mt-4">
               <h4>Preferred Stack:</h4>
-              {techStackSkillIcons}
+              <div className="flex flex-wrap gap-4">
+                {techStackSkillIcons}
+              </div>
             </div>
-            <div className="flex flex-col items-center md:flex-row gap-2">
+            <div className="flex flex-col items-start md:flex-row gap-2 mt-4">
               <h4 className="">Additional Skills:</h4>
-              {additionalSkillIcons}
+              <div className="flex flex-wrap gap-4">
+                {additionalSkillIcons}
+              </div>
             </div>
           </div>
         </div>
